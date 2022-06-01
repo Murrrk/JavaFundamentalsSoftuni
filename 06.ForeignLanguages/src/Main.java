1f9d63610b7e266105d45e291e10e7f1ae328ce9
Murrrk <markandonian123@gmail.com>
2022-06-01 14:02:22 +0300

    ForeignLanguages

---
+++ 06.ForeignLanguages/src/Main.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        String l = scanner.nextLine();
+
+        if(l.equals("England")  || l.equals("USA"))
+        {
+            System.out.println("English");
+        }
+        else if(l.equals("Spain") || l.equals("Argentina") || l.equals("Mexico"))
+        {
+            System.out.println("Spanish");
+        }
+        else
+        {
+            System.out.println("unknown");
+        }
+    }
+}
