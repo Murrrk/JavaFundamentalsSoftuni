617263e60e8d7f82bdea39fff2b16c76c18bebff
Murrrk <markandonian123@gmail.com>
2022-06-01 14:05:40 +0300

    Divisibleby3

---
+++ 08.Divisibleby3/src/Main.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        //String l = scanner.nextLine();
+        //int a = Integer.parseInt(scanner.nextLine());
+
+        for(int i = 3; i < 100; i+=3)
+        {
+            System.out.println(i);
+        }
+    }
+}
