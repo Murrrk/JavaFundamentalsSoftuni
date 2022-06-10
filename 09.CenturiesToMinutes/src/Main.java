818c169c5ffd0d04f8838d84400c90ca15ed7c0e
Murrrk <markandonian123@gmail.com>
2022-06-10 12:46:59 +0300

    Data Types and Variables - Lab

---
+++ 09.CenturiesToMinutes/src/Main.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        int c = Integer.parseInt(scan.nextLine());
+
+        double y = c * 100;
+        double d = y * 365.2422;
+        double h = d * 24;
+        double m = h * 60;
+
+        System.out.printf("%d centuries = %.0f years = %.0f Of days =" +
+                "%.0f Of hours = %.0f Of minutes", c,y,d,h,m);
+    }
+}
