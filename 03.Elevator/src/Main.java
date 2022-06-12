d628a9c720abf7448a9c5ec34b09a00d4c430e5a
Murrrk <markandonian123@gmail.com>
2022-06-12 21:18:17 +0300

    Data Types and Variables - Exercise

---
+++ 03.Elevator/src/Main.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int countPeople = Integer.parseInt(scanner.nextLine());
+        int capacity = Integer.parseInt(scanner.nextLine());
+
+        //курсове = хората / капацитета -> дробно число -> нагоре -> отпечатвам
+        double courses = Math.ceil(countPeople * 1.0 / capacity);
+        System.out.printf("%.0f", courses);
+
+    }
+}
