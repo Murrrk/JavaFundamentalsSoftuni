2748681d2f78fb266ac22f753d0dbf24dd9e9c3d
Murrrk <markandonian123@gmail.com>
2022-07-13 17:58:41 +0300

    Methods - Lab

---
+++ 06.CalculateRectangleArea/src/com/company/Main.java
@@ -0,0 +1,20 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        double widthInput = Double.parseDouble(scanner.nextLine());
+        double lengthInput = Double.parseDouble(scanner.nextLine());
+
+        double area = calculateRectangleArea(widthInput, lengthInput);
+
+        System.out.printf("%.0f", area);
+    }
+
+    public static double calculateRectangleArea(double width, double length) {
+        return width * length;
+    }
+}
